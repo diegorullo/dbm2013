a8b3e1e476b0bced56419150ea8b3324648d9c0a
stefania.buccoliero@gmail.com <stefania.buccoliero@gmail.com@22b074c5-2d81-8dd0-1a27-45971b5c5bce>
2013-04-09 19:10:01 +0000

    evoluzione task_1 : prototipo iniziale 9/4/13 (Stex)

--- trunk/DBM2013/src/lab1/PrintPaperVector.java
+++ trunk/DBM2013/src/lab1/PrintPaperVector.java
@@ -8,34 +8,115 @@ import java.util.Scanner;
 
 import com.mysql.jdbc.Statement;
 
-public class PrintPaperVector {
+public class PrintPaperVector 
+{
 	
+	private static Scanner input;
+
 	/* Task 1:
 	    - tokenizare
 		- stemming
 		- eliminare le stop words
 		- calcolo di tf e tfidf per ogni keyword
 		- costruzione del keyword vector <keyword, weight>
-		
-		BLA BLA BLA !!!
 	*/
 	
+	private static ResultSet queryDB (int paperid) 
+	{
+		Connection conn = null;
+		try 
+		{
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dblp", "root", "root");
+		} 
+		catch (SQLException e) 
+		{
+			System.out.println("SQLException : Impossibile connettersi al DB");
+		}
+		Statement stmt = null;
+		try 
+		{
+			stmt = (Statement) conn.createStatement();
+		} 
+		catch (SQLException e) 
+		{
+			System.out.println("SQLException : Impossibile creare la query");
+		}
+		
+		String query = "SELECT abstract FROM papers WHERE paperid = " + paperid;
+		ResultSet res = null;
+		try {
+			res = stmt.executeQuery(query);
+		} 
+		catch (SQLException e) 
+		{
+			System.out.println("SQLException : Impossibile eseguire la query");
+		}
+		
+		return res;
+	}
+	
+	/*public static String removeStopWordsAndStem(String input) throws IOException {
+	    Set<String> stopWords = new HashSet<String>();
+	    stopWords.add("a");
+	    stopWords.add("I");
+	    stopWords.add("the");
+
+	    TokenStream tokenStream = new StandardTokenizer(
+	            Version.LUCENE_30, new StringReader(input));
+	    tokenStream = new StopFilter(true, tokenStream, stopWords);
+	    tokenStream = new PorterStemFilter(tokenStream);
+
+	    StringBuilder sb = new StringBuilder();
+	    TermAttribute termAttr = tokenStream.getAttribute(TermAttribute.class);
+	    while (tokenStream.incrementToken()) {
+	        if (sb.length() > 0) {
+	            sb.append(" ");
+	        }
+	        sb.append(termAttr.term());
+	    }
+	    return sb.toString();
+	}*/
+	
+	// calcolo di TF per ogni keyword
+	private static void key_TF (ResultSet paper)
+	{}
+	
+	// calcolo di TFID per ogni keyword
+	private static void key_TFID (ResultSet paper)
+	{}
+
 	public static void main(String args[]) throws SQLException
 	{
-		while(true) {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Inserisci il codice autore: ");
-		int personid = input.nextInt();
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dblp", "root", "root");
-			Statement stmt = (Statement) conn.createStatement();
-			String query = "SELECT name FROM authors WHERE personid = " + personid;
-			ResultSet res = stmt.executeQuery(query);
-			while (res.next()) {
+		input = new Scanner(System.in);
+		
+		while(true)
+		{
+		
+		System.out.println("Inserisci il codice dell'articolo: ");
+		int paperid = input.nextInt();
+		
+		System.out.println("Inserisci il modello per i pesi (TF oppure TFID): ");
+		String model = input.next();
+		
+		ResultSet res = queryDB (paperid);
+			
+			
+			if(model.equalsIgnoreCase("TF"))
+			{
+				key_TF (res);
+			}
+			
+			else if(model.equalsIgnoreCase("TFID"))
+			{
+				key_TFID (res);
+			}
+			
+			else System.out.print("Modello per i pesi ERRATO !!!");
+			
+			/*while (res.next()) {
 				String name = res.getString("name");
 				System.out.println("Hello, " + name + "!");
-			}		
+			}*/		
 		}
     }
-
-	
 }
