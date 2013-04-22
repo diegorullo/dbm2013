d9f0c1beffb19bd4b701bd4b7e102ef00adfb23b
luca.violanti@gmail.com <luca.violanti@gmail.com@22b074c5-2d81-8dd0-1a27-45971b5c5bce>
2013-04-22 11:44:25 +0000

    Aggiunti metodi di supporto per il task 3: -

--- trunk/DBM2013/src/dblp/Corpus.java
+++ trunk/DBM2013/src/dblp/Corpus.java
@@ -3,8 +3,11 @@ package dblp;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+
+import javax.naming.NameNotFoundException;
 
 public class Corpus {
 
@@ -20,30 +23,99 @@ public class Corpus {
 		this.cardinality = cardinality;
 	}
 	
-	
+	// calcola l'idf di una keyword
 	public double getIDF(String s) throws Exception {
 		double idf = 0;
 		int m = 0;
 		int N = this.getCardinality();
 
-			//contare il numero di occorrenze della keyword s nel corpus
-			for(Paper p : papers)
-			{
-				HashMap<String, Integer> keywordSet = p.getKeywordSet();
-				Iterator<Entry<String, Integer>> it = keywordSet.entrySet().iterator();
-				while (it.hasNext()) {
-					Map.Entry<String, Integer> k = (Map.Entry<String, Integer>) it.next();
-					if (k.getKey().equals(s)) {
-						m += k.getValue();
-					}
+		// conta il numero di occorrenze della keyword s nel corpus
+		for(Paper p : papers) {
+			HashMap<String, Integer> keywordSet = p.getKeywordSet();
+			Iterator<Entry<String, Integer>> it = keywordSet.entrySet().iterator();
+			while (it.hasNext()) {
+				Map.Entry<String, Integer> k = (Map.Entry<String, Integer>) it.next();
+				if (k.getKey().equals(s)) {
+					m += k.getValue();
 				}
-					
-			}
-			if (N > 0 && m > 0)
-			idf = Math.log((double)N/m);
-			return idf;
+			}				
+		}
+		if (N > 0 && m > 0)
+		idf = Math.log((double)N/m);
+		return idf;
 	}
 	
+	// restituisce l'oggetto Author associato al nome dato
+	public Author getAuthorByName(String name) throws NameNotFoundException {
+		for (Author a : authors) {
+			if (name.equals(a.getName())) {
+				return a;
+			}
+		}
+		throw new NameNotFoundException("There is no author named '" + name + "' in the Corpus.");
+	}
+	
+	// estrae i nomi dei coautori di un autore dato
+	public List<String> getCoAuthorsNames(Author a) {
+		List<String> coAuthorsNames = new ArrayList<String>();
+		
+		for (Paper p : a.getPapers()) {
+			for (String coA : p.getAuthors()) {
+				if(!coAuthorsNames.contains(coA)) {
+					coAuthorsNames.add(coA);
+				}
+			}
+		}		
+		return coAuthorsNames;
+	}
+	
+	// estrae i coautori di un autore dato
+	public List<Author> getCoAuthors(Author a) throws NameNotFoundException {
+		List<Author> coAuthors = new ArrayList<Author>();
+		List<String> coAuthorsNames = this.getCoAuthorsNames(a);
+		for (String coA : coAuthorsNames) {
+			coAuthors.add(getAuthorByName(coA));
+		}
+		return coAuthors;
+	}
+	
+	// estrae la lista dei paper comuni ad una lista di autori (corpus ristretto)
+	public List<Paper> getRestrictedCorpus(List<Author> authors) {
+		List<Paper> papers = new ArrayList<Paper>();
+		
+		for (Author a : authors) {
+			for (Paper p : a.getPapers()) {
+				if (!papers.contains(p)) {
+					papers.add(p);
+				}
+			}
+		}
+		return papers;
+	} 
+	
+	// conta il numero di occorrenze della keyword s nel corpus ristretto
+	public double getRestrictedIDF(String s, List<Author> authors) {
+		double idf = 0;
+		int m = 0;
+		List<Paper> rc = getRestrictedCorpus(authors);
+		int N = rc.size();		
+
+		// conta il numero di occorrenze della keyword s nel corpus ristretto		
+		for(Paper p : rc) {
+			HashMap<String, Integer> keywordSet = p.getKeywordSet();
+			Iterator<Entry<String, Integer>> it = keywordSet.entrySet().iterator();
+			while (it.hasNext()) {
+				Map.Entry<String, Integer> k = (Map.Entry<String, Integer>) it.next();
+				if (k.getKey().equals(s)) {
+					m += k.getValue();
+				}
+			}				
+		}
+		if (N > 0 && m > 0) {
+			idf = Math.log((double)N/m);
+		}
+		return idf;
+	}
 	
 	public ArrayList<Author> getAuthors() {
 		return authors;
