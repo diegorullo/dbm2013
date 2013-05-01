356540356b6552ade35c7a82eb77bdbc354f6796
diego.rullo@gmail.com <diego.rullo@gmail.com@22b074c5-2d81-8dd0-1a27-45971b5c5bce>
2013-05-01 22:24:46 +0000

    Iniziato javadoc classe Paper e implementazione alcuni metodi di PaperTest

--- trunk/DBM2013/test/dblp/PaperTest.java
+++ trunk/DBM2013/test/dblp/PaperTest.java
@@ -1,10 +1,15 @@
 package dblp;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -25,21 +30,99 @@ public class PaperTest {
 		db.shutdown();
 	}
 
+	/** 
+	 * versione dummy test del metodo getKeywordSet
+	 * verifica la mera creazione dell'HashMap di keyword
+	 * In realtà, se il paper ha titolo ed abstract nullo, 
+	 * il test fallisce: l'HashMap viene creato ma rimane nullo.
+	 * 
+	 * @throws SQLException
+	 * @throws IOException
+	 */
 	@Test
-	public void testGetKeywordSet() throws SQLException, IOException {		
+	public void testGetKeywordSetDummy() throws SQLException, IOException {		
 		int paperid = 943390;
 		Paper paper = db.newPaper(paperid);
 		Map<String, Integer> ks = paper.getKeywordSet();
-		org.junit.Assert.assertNotNull("keywordset paper "+ paperid + "creato correttamente", ks);
-		
-//FIXME: se il paper non ha titolo nè abstract, ks è nullo 
-//		e l'asserzione fallisce...verificare
-		
+		org.junit.Assert.assertNotNull("keywordset paper "+ paperid + "creato correttamente", ks);		
+	}
+	
+	/**
+	 * testa il metodo getKeywordSet istanziando un paper
+	 * di keyword note.
+	 * 
+	 * @throws SQLException
+	 * @throws IOException
+	 */
+	@Test
+	public void testGetKeywordSet() throws SQLException, IOException {		
+		ArrayList<String> authorsNames = new ArrayList<String>();
+		authorsNames.add("Stefania");
+		ArrayList<Integer> authors = new ArrayList<Integer>();
+		authors.add(2013);
+		ArrayList<String> keywords = new ArrayList<String>();
+		keywords.add("media");
+		keywords.add("media");
+		keywords.add("keyword");
+		keywords.add("keyword");
+		keywords.add("keyword");
+		ArrayList<String> titlesKeywords = new ArrayList<String>();
+		titlesKeywords.add("testare");
+		titlesKeywords.add("TF");
+		titlesKeywords.add("uno");
+		Paper paper = new Paper(1, "Testare i TF a uno", 2013, "Gruppo DBM DLS", "media media keyword keyword keyword", authorsNames, authors, keywords, titlesKeywords);
+		Map<String, Integer> ks = paper.getKeywordSet(); 
+		Map<String, Integer> mapAtteso = new HashMap<String, Integer>() {
+		private static final long serialVersionUID = 1L;
+		{		    
+		    put("TF", 1*3);
+		    put("keyword", 3);
+		    put("media", 2);
+		    put("uno", 1*3);
+		    put("testare", 1*3);
+		}};
+		org.junit.Assert.assertEquals(mapAtteso, ks);	
 	}
 
 	@Test
 	public void testGetTF() {
-		fail("Not yet implemented");
+		ArrayList<String> authorsNames = new ArrayList<String>();
+		authorsNames.add("Stefania");
+		ArrayList<Integer> authors = new ArrayList<Integer>();
+		authors.add(2013);
+		ArrayList<String> keywords = new ArrayList<String>();
+		keywords.add("media");
+		keywords.add("media");
+		keywords.add("keyword");
+		keywords.add("keyword");
+		keywords.add("keyword");
+		ArrayList<String> titlesKeywords = new ArrayList<String>();
+		titlesKeywords.add("testare");
+		titlesKeywords.add("TF");
+		titlesKeywords.add("uno");
+		Paper paper = new Paper(1, "Testare i TF a uno", 2013, "Gruppo DBM DLS", "media media keyword keyword keyword", authorsNames, authors, keywords, titlesKeywords);
+		Map<String, Integer> ks = paper.getKeywordSet(); 
+//		Map<String, Integer> mapAtteso = new TreeMap<String, Double>() {
+//		private static final long serialVersionUID = 1L;
+//		{		    
+//		    put("TF", 1*3);
+//		    put("keyword", 3);
+//		    put("media", 2);
+//		    put("uno", 1*3);
+//		    put("testare", 1*3);
+//		}};
+//
+//		Iterator<Entry<String, Integer>> it = ks.entrySet().iterator();
+//		while(it.hasNext()) {
+//			Map.Entry<String, Integer> k = (Map.Entry<String, Integer>) it.next();
+//			mapAtteso.put(paper.getTF(k.getKey()), paper.getKeywordSet().get(k.getValue()));
+//		}
+//		
+//		double epsilon = (double)1/1000000000;
+//		assertEquals("La somma dei tf per le varie keyword vale 1.", 1.0, uno, epsilon);
+//		org.junit.Assert.assertEquals(mapAtteso, ks);	
+//
+//		}
 	}
 
 	@Test
