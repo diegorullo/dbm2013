7bd12ca44b0a180abd06f0ba9e25752a8fba5285
luca.violanti <luca.violanti@gmail.com>
2013-05-07 22:00:41 +0000

    Corretto il test su getRestrictedTF

--- DBM2013/test/dblp/AuthorTest.java
+++ DBM2013/test/dblp/AuthorTest.java
@@ -25,102 +25,116 @@ public class AuthorTest {
 	
 	@Test
 	public void testAuthor() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testGetWTFVector() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testGetWTFIDFVector() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testGetCombinedKeywordSet() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 
+	/**
+	 * 1 autore, 2 paper, la keyword "algorithm" compare in
+	 * entrambi i paper;
+	 * (vedi commento interno per il valore atteso)
+	 */
 	@Test
 	public void testGetRestrictedTF() {
-		//FIXME
-		ArrayList<String> authorsNames = new ArrayList<String>();
-		authorsNames.add("Stefania");
-		ArrayList<Integer> authors = new ArrayList<Integer>();
-		authors.add(2013);
+		
+		ArrayList<String> authorsNames1 = new ArrayList<String>();
+		authorsNames1.add("Stefania");
+		ArrayList<Integer> authors1 = new ArrayList<Integer>();
+		authors1.add(2013);
 		
 		// paper 1
-		ArrayList<String> keywords = new ArrayList<String>();
-		keywords.add("media");
-		keywords.add("media");
-		keywords.add("keyword");
-		keywords.add("keyword");
-		keywords.add("keyword");
-		ArrayList<String> titlesKeywords = new ArrayList<String>();
-		titlesKeywords.add("testare");
-		titlesKeywords.add("TF");
-		titlesKeywords.add("uno");
-		Paper paper = new Paper(1, "Testare i TF a uno", 2013, "Gruppo DBM DLS", "media media keyword keyword keyword", authorsNames, authors, keywords, titlesKeywords);
+		ArrayList<String> keywords1 = new ArrayList<String>();
+		keywords1.add("media");
+		keywords1.add("media");
+		keywords1.add("keyword");
+		keywords1.add("keyword");
+		keywords1.add("keyword");
+		ArrayList<String> titlesKeywords1 = new ArrayList<String>();
+		titlesKeywords1.add("testare");
+		titlesKeywords1.add("TF");
+		titlesKeywords1.add("uno");
+		Paper paper1 = new Paper(1, "Testare i TF a uno", 2013, "Gruppo DBM DLS", "media media keyword keyword keyword", authorsNames1, authors1, keywords1, titlesKeywords1);
 		
 		// paper 2
 		ArrayList<String> keywords2 = new ArrayList<String>();
-		keywords.add("algorithm");
-		keywords.add("algorithm");
-		keywords.add("parser");
-		keywords.add("parser");
+		keywords2.add("algorithm");
+		keywords2.add("algorithm");
+		keywords2.add("parser");
+		keywords2.add("parser");
 		ArrayList<String> titlesKeywords2 = new ArrayList<String>();
-		titlesKeywords.add("calcolare");
-		titlesKeywords.add("insieme");
-		titlesKeywords.add("degli");
-		titlesKeywords.add("articoli");
-		Paper paper2 = new Paper(2, "Calcolare insieme degli articoli", 2013, "Gruppo DBM DLS", "algorithm algorithm parser parser", authorsNames, authors, keywords2, titlesKeywords2);
-		ArrayList<Paper> paperList = new ArrayList<Paper>();
-		paperList.add(paper);
-		paperList.add(paper2);
-		Author a = new Author(2013, "Stefania", paperList);	
+		titlesKeywords2.add("calcolare");
+		titlesKeywords2.add("insieme");
+		titlesKeywords2.add("degli");
+		titlesKeywords2.add("articoli");
+		Paper paper2 = new Paper(2, "Calcolare insieme degli articoli", 2013, "Gruppo DBM DLS", "algorithm algorithm parser parser", authorsNames1, authors1, keywords2, titlesKeywords2);
+		
+		ArrayList<Paper> paperList1 = new ArrayList<Paper>();
+		paperList1.add(paper1);
+		paperList1.add(paper2);
+		Author a = new Author(2013, "Stefania", paperList1);	
 		String keyword = "algorithm";
 		Double tf = 0.0;
+		
+		/*
+         *  "algorithm" compare 2 volte /
+		 *  (il paper 1 ha 5 kw dall'abstract + 3 (peso) * 3 kw dal titolo + 
+		 *   il paper 2 ha 4 kw dall'abstract + 3 (peso) * 4 kw dal titolo) =
+		 *  -------------------------------------------------------------
+		 *  valore atteso 2 / (14 + 16)
+		 */
 		Double valAtteso = (double) 2/30;
 		tf = a.getRestrictedTF(keyword);
-		System.out.println("Keyword: " + keyword +", TF = "+ tf);
+
 		assertEquals ("Keyword: " + keyword +", TF = "+ tf, valAtteso, tf, 1/1000000);
 	}
 
 	@Test
 	public void testGetAuthorID() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testGetName() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testGetPapers() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testSetAuthorID() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testSetName() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testSetPapers() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testEqualsObject() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 
 }
