553f1c81d6089173768d208d7aae191b30ce007d
luca.violanti <luca.violanti@gmail.com>
2013-05-08 09:16:06 +0000

    Aggiunto test sulla normalizzazione (fallisce!)

--- DBM2013/src/utils/Normalization.java
+++ DBM2013/src/utils/Normalization.java
@@ -1,9 +1,12 @@
 package utils;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.TreeMap;
 
 public class Normalization {
+	
 	/**
 	 * Controlla che la somma degli elementi di map sia uno (a meno di un epsilon)
 	 * @param map
@@ -30,5 +33,57 @@ public class Normalization {
 		}
 		
 		return result;
+	}
+	
+	/**
+	 * Normalizza la treemap data in input;
+	 * se l'input è già normalizzato, non fa nulla
+	 */
+	public static TreeMap<String, Double> normalizeTreeMap(TreeMap<String, Double> treemap) {
+		TreeMap<String, Double> normalizedTreeMap = new TreeMap<String, Double>();
+		
+		double epsilon = (double) 1/1000000;
+		
+		if(Normalization.isNormalized(treemap, epsilon)) {
+			System.out.println("La map in input è già normalizzata!");
+			normalizedTreeMap = treemap;
+		}
+		else {
+			double denominatore = 0.0;
+			for (Entry<String, Double> entry : treemap.entrySet()) {
+				denominatore += entry.getValue();
+			}
+			for (Entry<String, Double> entry : treemap.entrySet()) {
+				normalizedTreeMap.put(entry.getKey(), entry.getValue() / denominatore);
+			}
+		}
+		
+		return normalizedTreeMap;
+	}
+	
+	/**
+	 * Normalizza la hashmap data in input;
+	 * se l'input è già normalizzato, non fa nulla
+	 */
+	public static HashMap<String, Double> normalizeHashMap(HashMap<String, Double> hashmap) {
+		HashMap<String, Double> normalizedHashMap = new HashMap<String, Double>();
+		
+		double epsilon = (double) 1/1000000;
+		
+		if(Normalization.isNormalized(hashmap, epsilon)) {
+			System.out.println("La map in input è già normalizzata!");
+			normalizedHashMap = hashmap;
+		}
+		else {
+			double denominatore = 0.0;
+			for (Entry<String, Double> entry : hashmap.entrySet()) {
+				denominatore += entry.getValue();
+			}
+			for (Entry<String, Double> entry : hashmap.entrySet()) {
+				normalizedHashMap.put(entry.getKey(), entry.getValue() / denominatore);
+			}
+		}
+		
+		return normalizedHashMap;
 	}
 }
---
+++ DBM2013/test/utils/NormalizationTest.java
@@ -0,0 +1,116 @@
+package utils;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.TreeMap;
+
+import org.junit.Test;
+
+import dblp.Author;
+import dblp.Corpus;
+import dblp.Paper;
+
+public class NormalizationTest {
+
+	/**
+	 * 2 autori, 3 paper, 1 in comune
+	 * @throws Exception 
+	 */
+	@Test
+	public void testNormalizeTreeMap() throws Exception {
+		
+		// -- PAPER --
+		
+		ArrayList<String> authorsNames1 = new ArrayList<String>();
+		authorsNames1.add("Stefania");
+		ArrayList<Integer> authors1 = new ArrayList<Integer>();
+		authors1.add(1001);
+		
+		ArrayList<String> authorsNames2 = new ArrayList<String>();
+		authorsNames2.add("Luca");
+		ArrayList<Integer> authors2 = new ArrayList<Integer>();
+		authors2.add(1002);
+				
+		ArrayList<String> authorsNames3 = new ArrayList<String>();
+		authorsNames3.add("Stefania");
+		authorsNames3.add("Luca");
+		ArrayList<Integer> authors3 = new ArrayList<Integer>();
+		authors3.add(1001);
+		authors3.add(1002);
+		
+		// paper 1
+		ArrayList<String> keywords1 = new ArrayList<String>();
+		keywords1.add("media");
+		keywords1.add("media");
+		keywords1.add("keyword");
+		keywords1.add("keyword");
+		keywords1.add("keyword");
+		ArrayList<String> titlesKeywords1 = new ArrayList<String>();
+		titlesKeywords1.add("testare");
+		titlesKeywords1.add("TF");
+		titlesKeywords1.add("uno");
+		Paper paper1 = new Paper(1, "Testare i TF a uno", 2013, "Gruppo DBM DLS", "media media keyword keyword keyword", authorsNames1, authors1, keywords1, titlesKeywords1);
+		
+		// paper 2
+		ArrayList<String> keywords2 = new ArrayList<String>();
+		keywords2.add("algorithm");
+		keywords2.add("algorithm");
+		keywords2.add("parser");
+		keywords2.add("parser");
+		ArrayList<String> titlesKeywords2 = new ArrayList<String>();
+		titlesKeywords2.add("calcolare");
+		titlesKeywords2.add("insieme");
+		titlesKeywords2.add("degli");
+		titlesKeywords2.add("articoli");
+		Paper paper2 = new Paper(2, "Calcolare insieme degli articoli", 2013, "Gruppo DBM DLS", "algorithm algorithm parser parser", authorsNames1, authors1, keywords2, titlesKeywords2);
+		
+		// paper 3 (in comune)
+		ArrayList<String> keywords3 = new ArrayList<String>();
+		keywords3.add("program");
+		keywords3.add("algorithm");
+		keywords3.add("parser");
+		ArrayList<String> titlesKeywords3 = new ArrayList<String>();
+		titlesKeywords3.add("program");
+		titlesKeywords3.add("agile");
+		titlesKeywords3.add("method");
+		Paper paper3 = new Paper(3, "programming with agile method", 2013, "Gruppo DBM DLS", "program algorithm parser", authorsNames3, authors3, keywords3, titlesKeywords3);
+		
+		
+		// -- AUTORI --
+		
+		ArrayList<Paper> paperListStefania = new ArrayList<Paper>();
+		paperListStefania.add(paper1);
+		paperListStefania.add(paper3);
+		
+		ArrayList<Paper> paperListLuca = new ArrayList<Paper>();
+		paperListLuca.add(paper2);
+		paperListLuca.add(paper3);
+
+		Author authorStefania = new Author(1001, "Stefania", paperListStefania);
+		Author authorLuca = new Author(1002, "Luca", paperListLuca);
+		
+		
+		// -- CORPUS
+		
+		ArrayList<Author> listaAutoriNelCorpus = new ArrayList<Author>();
+		listaAutoriNelCorpus.add(authorStefania);
+		listaAutoriNelCorpus.add(authorLuca);
+		
+		ArrayList<Paper> listaPaperNelCorpus = new ArrayList<Paper>();
+		listaPaperNelCorpus.add(paper1);
+		listaPaperNelCorpus.add(paper2);
+		listaPaperNelCorpus.add(paper3);		
+
+		Corpus dummyCorpus = new Corpus(listaAutoriNelCorpus, listaPaperNelCorpus, listaPaperNelCorpus.size());
+		
+		TreeMap<String, Double> vettore = (TreeMap<String, Double>) paper1.getTFIDFVector(dummyCorpus);
+		
+		assertFalse(Normalization.isNormalized(vettore, 1/1000000));
+		TreeMap<String, Double> vettoreNormalizzato = Normalization.normalizeTreeMap(vettore);
+		assertTrue(Normalization.isNormalized(vettoreNormalizzato, 1/1000000));
+		
+	}
+
+}
