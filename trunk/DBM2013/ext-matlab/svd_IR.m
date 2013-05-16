ba836945ae1295448a6d5d423980386bfd861efa
stefania.buccoliero@gmail.com <stefania.buccoliero@gmail.com@22b074c5-2d81-8dd0-1a27-45971b5c5bce>
2013-05-16 20:32:24 +0000

    Applicata la "trasposta" alla matrice V della SVD in svd_IR.

--- trunk/DBM2013/ext-matlab/svd_IR.m
+++ trunk/DBM2013/ext-matlab/svd_IR.m
@@ -3,5 +3,5 @@ X = csvread('../../data/X.csv');
 [U,S,V] = svd(X,'econ');
 csvwrite('../../data/U.csv',U);
 csvwrite('../../data/S.csv',S);
-csvwrite('../../data/V.csv',V);
+csvwrite('../../data/V.csv',V');
 end
