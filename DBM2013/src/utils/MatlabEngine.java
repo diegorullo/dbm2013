b6a3c7609632e8d32e89729b037b76df32e9cb62
stefania.buccoliero <stefania.buccoliero@gmail.com>
2013-05-16 20:10:42 +0000

    Creata classe MatlabEngine (utils) per gestione funzioni SVD e PCA

---
+++ DBM2013/src/utils/MatlabEngine.java
@@ -0,0 +1,52 @@
+package utils;
+
+import matlabcontrol.*;
+
+public class MatlabEngine 
+{
+	
+	private MatlabProxyFactory factory;
+	private MatlabProxy proxy;
+
+	/**
+	 * Crea un proxy, che useremo per controllare MATLAB e ne imposta il path
+	 * @param args
+	 * @throws MatlabConnectionException 
+	 * @throws MatlabInvocationException 
+	*/
+	public void init() throws MatlabConnectionException, MatlabInvocationException
+	{
+		factory = new MatlabProxyFactory();
+		proxy = factory.getProxy();
+		
+		//matlab path
+		String path = "cd(\'*******\')";
+		proxy.eval(path);
+	}
+	
+	/**
+	 * Esegue la funzione svd_IR 
+	 * @throws MatlabInvocationException 
+	*/
+	public void svd_IR() throws MatlabInvocationException 
+	{
+		proxy.eval("svd_IR");
+	}
+	
+	/**
+	 * Esegue la funzione pca_IR 
+	 * @throws MatlabInvocationException 
+	*/
+	public void pca_IR() throws MatlabInvocationException 
+	{
+		proxy.eval("pca_IR");
+	}
+	
+	/**
+	 * disconnette il proxy da Matlab
+	 */
+	public void shutdown() 
+	{
+		proxy.disconnect();
+	}
+}
