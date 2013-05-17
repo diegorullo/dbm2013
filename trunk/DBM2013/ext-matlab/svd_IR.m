66ab1f277e1150828d750364452734fcaf685f34
luca.violanti@gmail.com <luca.violanti@gmail.com@22b074c5-2d81-8dd0-1a27-45971b5c5bce>
2013-05-17 16:41:07 +0000

    [committato con errori] Aggiunti metodo eval alla classe MatlabEngine (prima era un blocco unico con init); creata classe IO in cui si fanno le operazioni di stampa/lettura da file, in cui è stato spostato il metodo già presente in Printer; corretti i test corrispondenti;

--- trunk/DBM2013/ext-matlab/svd_IR.m
+++ trunk/DBM2013/ext-matlab/svd_IR.m
@@ -1,7 +1,7 @@
 function svd_IR
-X = csvread('../../data/X.csv');
+X = csvread('../data/X.csv');
 [U,S,V] = svd(X,'econ');
-csvwrite('../../data/U.csv',U);
-csvwrite('../../data/S.csv',S);
-csvwrite('../../data/V.csv',V');
+csvwrite('../data/U.csv',U);
+csvwrite('../data/S.csv',S);
+csvwrite('../data/V.csv',V');
 end
--- trunk/DBM2013/test/utils/MatlabEngineTest.java
+++ trunk/DBM2013/test/utils/MatlabEngineTest.java
@@ -1,11 +1,7 @@
 package utils;
 
-import static org.junit.Assert.*;
-
 import matlabcontrol.MatlabConnectionException;
 import matlabcontrol.MatlabInvocationException;
-import matlabcontrol.MatlabProxy;
-import matlabcontrol.MatlabProxyFactory;
 
 import org.junit.Test;
 
@@ -15,5 +11,8 @@ public class MatlabEngineTest {
 	public void initTest() throws MatlabConnectionException, MatlabInvocationException {
 		MatlabEngine me = new MatlabEngine();
 			me.init();
+			
+			me.eval("svd_IR");
+			me.eval("pca_IR");
 	}
 }
