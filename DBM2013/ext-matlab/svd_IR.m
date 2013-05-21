14bd89a5f6cce95c00d1ed9f8fcd622ac76858a5
luca.violanti <luca.violanti@gmail.com>
2013-05-21 21:11:36 +0000

    Corretto test

--- DBM2013/ext-matlab/svd_IR.m
+++ DBM2013/ext-matlab/svd_IR.m
@@ -1,8 +1,8 @@
 function svd_IR(filename)
-path = strcat('../../data/',filename)
-X = csvread(path);
+functionpath = strcat('../data/',filename);
+X = csvread(functionpath);
 [U,S,V] = svd(X,'econ');
-csvwrite(strcat('../../data/U_',filename),U);
-csvwrite(strcat('../../data/S_',filename),S);
-csvwrite(strcat('../../data/V_',filename),V');
+csvwrite(strcat('../data/U_',filename),U);
+csvwrite(strcat('../data/S_',filename),S);
+csvwrite(strcat('../data/V_',filename),V');
 end
--- DBM2013/test/utils/MatlabEngineTest.java
+++ DBM2013/test/utils/MatlabEngineTest.java
@@ -16,47 +16,48 @@ import dblp.Corpus;
 
 public class MatlabEngineTest {
 	
-	private final static boolean DEBUG = false;
+	private final static boolean DEBUG = true;
 	
 	static DBEngine db = new DBEngine();
 	static MatlabEngine me = new MatlabEngine();
-//	@BeforeClass
-//	public static void testSetup() throws SQLException, MatlabConnectionException, MatlabInvocationException {
-//		db.init();
-//		me.init();
-//	}
-//
-//	@AfterClass
-//	public static void testCleanup() throws SQLException, MatlabInvocationException {
-//		db.shutdown();
-//		me.shutdown();
-//	}
+	@BeforeClass
+	public static void testSetup() throws SQLException, MatlabConnectionException, MatlabInvocationException {
+		db.init();
+		me.init();
+	}
+
+	@AfterClass
+	public static void testCleanup() throws SQLException, MatlabInvocationException {
+		db.shutdown();
+		me.shutdown();
+	}
 
 	
 	@Test
 	public void evalTestDummy() throws MatlabConnectionException, MatlabInvocationException {
-		if(true) {		
-			me.init();
-			me.eval("svd_IR","Stefania" + ".csv");
-			me.eval("pca_IR","Stefania" + ".csv");
-			//me.shutdown();
+		if(DEBUG) {
+			me.eval("svd_IR","Stefania.csv");
+			me.eval("pca_IR","Stefania.csv");
 		}
 	}
 	
 	@Test
 	public void evalTestSVD() throws Exception {
-		if(DEBUG)
-		{
-		Corpus dblp = db.newCorpus();
-		Author testAuthor = dblp.getAuthorByID(2390072);
-		String fileName = testAuthor.getAuthorID() + ".csv";
-		ArrayList<TreeMap<String, Double>> documentTermMatrix = dblp.getDocumentTermMatrix(testAuthor);
-		IO.printDocumentTermMatrixOnFile(documentTermMatrix, "../data/" + fileName);
-		System.out.println("../data/" + fileName);
-		me.eval("svd_IR",fileName);
-		ArrayList<ArrayList<Double>> matrix_v =IO.readDocumentTermMatrixFromFile("../data/" +"V" + ".csv");
-		System.out.println("Matrice V:");
-		Printer.printMatrix(matrix_v);
+		if(DEBUG) {
+			Corpus dblp = db.newCorpus();
+			Author testAuthor = dblp.getAuthorByID(2390072);
+			
+			String fileName = testAuthor.getAuthorID() + ".csv";
+			ArrayList<TreeMap<String, Double>> documentTermMatrix = dblp.getDocumentTermMatrix(testAuthor);
+			
+			IO.printDocumentTermMatrixOnFile(documentTermMatrix, "../data/" + fileName);
+			System.out.println("Scritto su file: " + fileName);
+			
+			me.eval("svd_IR",fileName);
+			ArrayList<ArrayList<Double>> matrix_v = IO.readDocumentTermMatrixFromFile("../data/" + testAuthor.getAuthorID() + ".csv");
+
+			System.out.println("Letta matrice V da file:");
+			Printer.printMatrix(matrix_v);
 		}
 	}
 }
