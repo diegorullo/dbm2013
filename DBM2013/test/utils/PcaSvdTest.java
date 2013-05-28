a4ffb0e036b7095e92918d3a0f44161a263d3ffb
stefania.buccoliero <stefania.buccoliero@gmail.com>
2013-05-28 10:51:06 +0000

    Implementato metodo getTopN per estrarre le top da PCA e SVD (Author). Creata classe PcaSvdTest con test su getTopN.

---
+++ DBM2013/test/utils/PcaSvdTest.java
@@ -0,0 +1,185 @@
+package utils;
+
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+import org.junit.Test;
+
+import dblp.Author;
+import dblp.Corpus;
+import dblp.Paper;
+
+public class PcaSvdTest 
+{
+	
+	/**
+	 * 1 autore, 3 paper
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testGetTop5SVDDummy() throws Exception {
+
+		// -- PAPER --
+
+		ArrayList<String> authorsNames1 = new ArrayList<String>();
+		authorsNames1.add("Stefania");
+		ArrayList<Integer> authors1 = new ArrayList<Integer>();
+		authors1.add(1001);
+
+		// paper 1
+		ArrayList<String> keywords1 = new ArrayList<String>();
+		keywords1.add("media");
+		keywords1.add("media");
+		keywords1.add("keyword");
+		keywords1.add("keyword");
+		keywords1.add("keyword");
+		ArrayList<String> titlesKeywords1 = new ArrayList<String>();
+		titlesKeywords1.add("testare");
+		titlesKeywords1.add("TF");
+		titlesKeywords1.add("uno");
+		Paper paper1 = new Paper(1, "Testare i TF a uno", 2013,
+				"Gruppo DBM DLS", "media media keyword keyword keyword",
+				authorsNames1, authors1, keywords1, titlesKeywords1);
+
+		// paper 2
+		ArrayList<String> keywords2 = new ArrayList<String>();
+		keywords2.add("algorithm");
+		keywords2.add("algorithm");
+		keywords2.add("parser");
+		keywords2.add("parser");
+		ArrayList<String> titlesKeywords2 = new ArrayList<String>();
+		titlesKeywords2.add("calcolare");
+		titlesKeywords2.add("insieme");
+		titlesKeywords2.add("degli");
+		titlesKeywords2.add("articoli");
+		Paper paper2 = new Paper(2, "Calcolare insieme degli articoli", 1900,
+				"Gruppo DBM DLS", "algorithm algorithm parser parser",
+				authorsNames1, authors1, keywords2, titlesKeywords2);
+
+		// paper 3 (in comune)
+		ArrayList<String> keywords3 = new ArrayList<String>();
+		keywords3.add("program");
+		keywords3.add("algorithm");
+		keywords3.add("parser");
+		ArrayList<String> titlesKeywords3 = new ArrayList<String>();
+		titlesKeywords3.add("program");
+		titlesKeywords3.add("agile");
+		titlesKeywords3.add("method");
+		Paper paper3 = new Paper(3, "programming with agile method", 2013,
+				"Gruppo DBM DLS", "program algorithm parser", authorsNames1,
+				authors1, keywords3, titlesKeywords3);
+
+		// -- AUTORI --
+
+		ArrayList<Paper> paperListStefania = new ArrayList<Paper>();
+		paperListStefania.add(paper1);
+		paperListStefania.add(paper2);
+		paperListStefania.add(paper3);
+
+		Author authorStefania = new Author(1001, "Stefania", paperListStefania);
+
+		// -- CORPUS
+
+		ArrayList<Author> listaAutoriNelCorpus = new ArrayList<Author>();
+		listaAutoriNelCorpus.add(authorStefania);
+
+		ArrayList<Paper> listaPaperNelCorpus = new ArrayList<Paper>();
+		listaPaperNelCorpus.add(paper1);
+		listaPaperNelCorpus.add(paper2);
+		listaPaperNelCorpus.add(paper3);
+
+		Corpus dummyCorpus = new Corpus(listaAutoriNelCorpus,
+				listaPaperNelCorpus, listaPaperNelCorpus.size());
+
+		ArrayList<ArrayList<Double>> v_matrix = authorStefania.getSVD(dummyCorpus);
+		System.out.println("v_matrix" + v_matrix);
+		ArrayList<TreeMap<String, Double>> n_TopMatrix = authorStefania.getTopN(v_matrix, 5);
+		System.out.println("5_SVDTopMatrix"+n_TopMatrix);
+	}
+	/**
+	 * 1 autore, 3 paper
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testGetTop5PCADummy() throws Exception {
+
+		// -- PAPER --
+
+		ArrayList<String> authorsNames1 = new ArrayList<String>();
+		authorsNames1.add("Stefania");
+		ArrayList<Integer> authors1 = new ArrayList<Integer>();
+		authors1.add(1001);
+
+		// paper 1
+		ArrayList<String> keywords1 = new ArrayList<String>();
+		keywords1.add("media");
+		keywords1.add("media");
+		keywords1.add("keyword");
+		keywords1.add("keyword");
+		keywords1.add("keyword");
+		ArrayList<String> titlesKeywords1 = new ArrayList<String>();
+		titlesKeywords1.add("testare");
+		titlesKeywords1.add("TF");
+		titlesKeywords1.add("uno");
+		Paper paper1 = new Paper(1, "Testare i TF a uno", 2013,
+				"Gruppo DBM DLS", "media media keyword keyword keyword",
+				authorsNames1, authors1, keywords1, titlesKeywords1);
+
+		// paper 2
+		ArrayList<String> keywords2 = new ArrayList<String>();
+		keywords2.add("algorithm");
+		keywords2.add("algorithm");
+		keywords2.add("parser");
+		keywords2.add("parser");
+		ArrayList<String> titlesKeywords2 = new ArrayList<String>();
+		titlesKeywords2.add("calcolare");
+		titlesKeywords2.add("insieme");
+		titlesKeywords2.add("degli");
+		titlesKeywords2.add("articoli");
+		Paper paper2 = new Paper(2, "Calcolare insieme degli articoli", 1900,
+				"Gruppo DBM DLS", "algorithm algorithm parser parser",
+				authorsNames1, authors1, keywords2, titlesKeywords2);
+
+		// paper 3 (in comune)
+		ArrayList<String> keywords3 = new ArrayList<String>();
+		keywords3.add("program");
+		keywords3.add("algorithm");
+		keywords3.add("parser");
+		ArrayList<String> titlesKeywords3 = new ArrayList<String>();
+		titlesKeywords3.add("program");
+		titlesKeywords3.add("agile");
+		titlesKeywords3.add("method");
+		Paper paper3 = new Paper(3, "programming with agile method", 2013,
+				"Gruppo DBM DLS", "program algorithm parser", authorsNames1,
+				authors1, keywords3, titlesKeywords3);
+
+		// -- AUTORI --
+
+		ArrayList<Paper> paperListStefania = new ArrayList<Paper>();
+		paperListStefania.add(paper1);
+		paperListStefania.add(paper2);
+		paperListStefania.add(paper3);
+
+		Author authorStefania = new Author(1001, "Stefania", paperListStefania);
+
+		// -- CORPUS
+
+		ArrayList<Author> listaAutoriNelCorpus = new ArrayList<Author>();
+		listaAutoriNelCorpus.add(authorStefania);
+
+		ArrayList<Paper> listaPaperNelCorpus = new ArrayList<Paper>();
+		listaPaperNelCorpus.add(paper1);
+		listaPaperNelCorpus.add(paper2);
+		listaPaperNelCorpus.add(paper3);
+
+		Corpus dummyCorpus = new Corpus(listaAutoriNelCorpus,
+				listaPaperNelCorpus, listaPaperNelCorpus.size());
+
+		ArrayList<ArrayList<Double>> score_matrix = authorStefania.getPCA(dummyCorpus);
+		System.out.println("score_matrix" + score_matrix);
+		ArrayList<TreeMap<String, Double>> n_TopMatrix = authorStefania.getTopN(score_matrix, 5);
+		System.out.println("5_PCATopMatrix"+n_TopMatrix);
+	}
+}
