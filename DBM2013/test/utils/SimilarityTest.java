feda11715cb23fdefdb265a577d0dfdff2e3ddb5
luca.violanti <luca.violanti@gmail.com>
2013-05-28 20:36:38 +0000

    Rimossi import; Commentati metodi di stampa

--- DBM2013/test/utils/SimilarityTest.java
+++ DBM2013/test/utils/SimilarityTest.java
@@ -14,7 +14,7 @@ public class SimilarityTest {
 	
 	@Test
 	public void testGetCosineSimilarityWithSelf() throws Exception {
-		if (!DEBUG) {
+		if (DEBUG) {
 			Factory f = new Factory();
 			Corpus dblp = f.getCorpus();
 			
@@ -35,17 +35,17 @@ public class SimilarityTest {
 			Corpus dblp = f.getCorpus();
 			//Autore "Maria Luisa Sapino"
 			Author authorSapino = dblp.getAuthorByID(1677020);
-			System.out.println("KS di Sapino (" + authorSapino.getKeywordSet().size() + "): " + authorSapino.getKeywordSet());
+//			System.out.println("KS di Sapino (" + authorSapino.getKeywordSet().size() + "): " + authorSapino.getKeywordSet());
 			
 			//Autore "K. Selcuk Candan" (esiste anche un "K.S. Candan" che pero' non ha papers)
 			Author authorCandan = dblp.getAuthorByID(1636579);
-			System.out.println("KS di Candan (" + authorCandan.getKeywordSet().size() + "): " + authorCandan.getKeywordSet());
+//			System.out.println("KS di Candan (" + authorCandan.getKeywordSet().size() + "): " + authorCandan.getKeywordSet());
 			
 			double similarityCS = authorCandan.getSimilarityOnKeywordVector(authorSapino, dblp);
 			double similaritySC = authorSapino.getSimilarityOnKeywordVector(authorCandan, dblp);
 			
-			System.out.println("Similarità Candan-Sapino: " + similarityCS);
-			System.out.println("Similarità Sapino-Candan: " + similaritySC);
+//			System.out.println("Similarità Candan-Sapino: " + similarityCS);
+//			System.out.println("Similarità Sapino-Candan: " + similaritySC);
 			
 			double epsilon = 1/1000000;			
 			assertEquals(similarityCS, similaritySC, epsilon);
