983704910d2b2defb3a5f5cc77501b0e177b62c0
luca.violanti <luca.violanti@gmail.com>
2013-06-04 13:56:47 +0000

    Corretti test

--- DBM2013/test/utils/PcaSvdTest.java
+++ DBM2013/test/utils/PcaSvdTest.java
@@ -1,38 +1,52 @@
 package utils;
 
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.TreeMap;
 
 import matlabcontrol.MatlabConnectionException;
 import matlabcontrol.MatlabInvocationException;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import dblp.Author;
 import dblp.Corpus;
+import dblp.Factory;
 import dblp.Paper;
 import exceptions.AuthorWithoutPapersException;
+import exceptions.NoAuthorsWithSuchIDException;
 
-public class PcaSvdTest 
-{
+public class PcaSvdTest {
 	
-	/**
-	 * 1 autore, 3 paper
-	 * @throws AuthorWithoutPapersException 
-	 * @throws MatlabInvocationException 
-	 * @throws MatlabConnectionException 
-	 * 
-	 * @throws Exception
-	 */
-	@Test
-	public void testGetTop5SVDDummy() throws MatlabConnectionException, MatlabInvocationException, AuthorWithoutPapersException {
-
+	private final static boolean DEBUG = true;
+	private final static boolean BIG_DATA_ALL = true;
+	private final static boolean BIG_DATA_ONE = true;
+	private final static boolean PRINT = true;
+	
+	static Author authorStefania;
+	static Author authorLuca;
+	static Corpus dummyCorpus;
+	static double epsilon = 1.0/1000000;
+	
+	static Author authorCandan;
+	static Author authorSapino;
+	static Corpus dblp;
+	
+	@BeforeClass	
+	public static void getDummyEnvironment() {
 		// -- PAPER --
 
 		ArrayList<String> authorsNames1 = new ArrayList<String>();
 		authorsNames1.add("Stefania");
 		ArrayList<Integer> authors1 = new ArrayList<Integer>();
 		authors1.add(1001);
+
+		ArrayList<String> authorsNames2 = new ArrayList<String>();
+		authorsNames2.add("Luca");
+		ArrayList<Integer> authors2 = new ArrayList<Integer>();
+		authors2.add(1002);
 
 		// paper 1
 		ArrayList<String> keywords1 = new ArrayList<String>();
@@ -60,11 +74,11 @@ public class PcaSvdTest
 		titlesKeywords2.add("insieme");
 		titlesKeywords2.add("degli");
 		titlesKeywords2.add("articoli");
-		Paper paper2 = new Paper(2, "Calcolare insieme degli articoli", 1900,
+		Paper paper2 = new Paper(2, "Calcolare insieme degli articoli", 2013,
 				"Gruppo DBM DLS", "algorithm algorithm parser parser",
 				authorsNames1, authors1, keywords2, titlesKeywords2);
 
-		// paper 3 (in comune)
+		// paper 3
 		ArrayList<String> keywords3 = new ArrayList<String>();
 		keywords3.add("program");
 		keywords3.add("algorithm");
@@ -74,30 +88,73 @@ public class PcaSvdTest
 		titlesKeywords3.add("agile");
 		titlesKeywords3.add("method");
 		Paper paper3 = new Paper(3, "programming with agile method", 2013,
-				"Gruppo DBM DLS", "program algorithm parser", authorsNames1,
-				authors1, keywords3, titlesKeywords3);
+				"Gruppo DBM DLS", "program algorithm parser", authorsNames2,
+				authors2, keywords3, titlesKeywords3);
+
+		// paper 4
+		ArrayList<String> keywords4 = new ArrayList<String>();
+		keywords4.add("method");
+		keywords4.add("token");
+		keywords4.add("parser");
+		keywords4.add("parser");
+		ArrayList<String> titlesKeywords4 = new ArrayList<String>();
+		titlesKeywords4.add("insieme");
+		titlesKeywords4.add("dei");
+		titlesKeywords4.add("token");
+		Paper paper4 = new Paper(4, "insieme dei token", 2013,
+				"Gruppo DBM DLS", "method token parser parser", authorsNames2,
+				authors2, keywords4, titlesKeywords4);
 
 		// -- AUTORI --
 
 		ArrayList<Paper> paperListStefania = new ArrayList<Paper>();
 		paperListStefania.add(paper1);
 		paperListStefania.add(paper2);
-		paperListStefania.add(paper3);
 
-		Author authorStefania = new Author(1001, "Stefania", paperListStefania);
+		ArrayList<Paper> paperListLuca = new ArrayList<Paper>();
+		paperListLuca.add(paper3);
+		paperListLuca.add(paper4);
+
+		authorStefania = new Author(1001, "Stefania", paperListStefania);
+		authorLuca = new Author(1002, "Luca", paperListLuca);
 
 		// -- CORPUS
 
 		ArrayList<Author> listaAutoriNelCorpus = new ArrayList<Author>();
 		listaAutoriNelCorpus.add(authorStefania);
+		listaAutoriNelCorpus.add(authorLuca);
 
 		ArrayList<Paper> listaPaperNelCorpus = new ArrayList<Paper>();
 		listaPaperNelCorpus.add(paper1);
 		listaPaperNelCorpus.add(paper2);
 		listaPaperNelCorpus.add(paper3);
+		listaPaperNelCorpus.add(paper4);
 
-		Corpus dummyCorpus = new Corpus(listaAutoriNelCorpus,
-				listaPaperNelCorpus, listaPaperNelCorpus.size());
+		dummyCorpus = new Corpus(listaAutoriNelCorpus,listaPaperNelCorpus, listaPaperNelCorpus.size());
+	}
+		
+	@BeforeClass
+	public static void getEnvironment() throws SQLException, MatlabConnectionException, MatlabInvocationException, IOException, NoAuthorsWithSuchIDException {
+		Factory f = new Factory();
+		dblp = f.getCorpus();
+		//Autore "Maria Luisa Sapino"
+		authorSapino = dblp.getAuthorByID(1677020);
+		
+		//Autore "K. Selcuk Candan" (esiste anche un "K.S. Candan" che pero' non ha papers)
+		authorCandan = dblp.getAuthorByID(1636579);
+		
+	}
+	
+	/**
+	 * 1 autore, 3 paper
+	 * @throws AuthorWithoutPapersException 
+	 * @throws MatlabInvocationException 
+	 * @throws MatlabConnectionException 
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testGetTop5SVDDummy() throws MatlabConnectionException, MatlabInvocationException, AuthorWithoutPapersException {
 
 		ArrayList<ArrayList<Double>> vMatrix = authorStefania.getSVD(dummyCorpus, 5);
 		System.out.println("v_matrix" + vMatrix);
@@ -114,78 +171,6 @@ public class PcaSvdTest
 	 */
 	@Test
 	public void testGetTop5PCADummy() throws MatlabConnectionException, MatlabInvocationException, AuthorWithoutPapersException {
-
-		// -- PAPER --
-
-		ArrayList<String> authorsNames1 = new ArrayList<String>();
-		authorsNames1.add("Stefania");
-		ArrayList<Integer> authors1 = new ArrayList<Integer>();
-		authors1.add(1001);
-
-		// paper 1
-		ArrayList<String> keywords1 = new ArrayList<String>();
-		keywords1.add("media");
-		keywords1.add("media");
-		keywords1.add("keyword");
-		keywords1.add("keyword");
-		keywords1.add("keyword");
-		ArrayList<String> titlesKeywords1 = new ArrayList<String>();
-		titlesKeywords1.add("testare");
-		titlesKeywords1.add("TF");
-		titlesKeywords1.add("uno");
-		Paper paper1 = new Paper(1, "Testare i TF a uno", 2013,
-				"Gruppo DBM DLS", "media media keyword keyword keyword",
-				authorsNames1, authors1, keywords1, titlesKeywords1);
-
-		// paper 2
-		ArrayList<String> keywords2 = new ArrayList<String>();
-		keywords2.add("algorithm");
-		keywords2.add("algorithm");
-		keywords2.add("parser");
-		keywords2.add("parser");
-		ArrayList<String> titlesKeywords2 = new ArrayList<String>();
-		titlesKeywords2.add("calcolare");
-		titlesKeywords2.add("insieme");
-		titlesKeywords2.add("degli");
-		titlesKeywords2.add("articoli");
-		Paper paper2 = new Paper(2, "Calcolare insieme degli articoli", 1900,
-				"Gruppo DBM DLS", "algorithm algorithm parser parser",
-				authorsNames1, authors1, keywords2, titlesKeywords2);
-
-		// paper 3 (in comune)
-		ArrayList<String> keywords3 = new ArrayList<String>();
-		keywords3.add("program");
-		keywords3.add("algorithm");
-		keywords3.add("parser");
-		ArrayList<String> titlesKeywords3 = new ArrayList<String>();
-		titlesKeywords3.add("program");
-		titlesKeywords3.add("agile");
-		titlesKeywords3.add("method");
-		Paper paper3 = new Paper(3, "programming with agile method", 2013,
-				"Gruppo DBM DLS", "program algorithm parser", authorsNames1,
-				authors1, keywords3, titlesKeywords3);
-
-		// -- AUTORI --
-
-		ArrayList<Paper> paperListStefania = new ArrayList<Paper>();
-		paperListStefania.add(paper1);
-		paperListStefania.add(paper2);
-		paperListStefania.add(paper3);
-
-		Author authorStefania = new Author(1001, "Stefania", paperListStefania);
-
-		// -- CORPUS
-
-		ArrayList<Author> listaAutoriNelCorpus = new ArrayList<Author>();
-		listaAutoriNelCorpus.add(authorStefania);
-
-		ArrayList<Paper> listaPaperNelCorpus = new ArrayList<Paper>();
-		listaPaperNelCorpus.add(paper1);
-		listaPaperNelCorpus.add(paper2);
-		listaPaperNelCorpus.add(paper3);
-
-		Corpus dummyCorpus = new Corpus(listaAutoriNelCorpus,
-				listaPaperNelCorpus, listaPaperNelCorpus.size());
 
 		ArrayList<ArrayList<Double>> scoreLatentMatrix = authorStefania.getPCA(dummyCorpus,5);
 		System.out.println("score_latent_matrix" + scoreLatentMatrix);
