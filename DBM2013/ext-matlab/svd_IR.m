6be60b7514a0acc58bab9f0c6904414e843623ee
luca.violanti <luca.violanti@gmail.com>
2013-06-05 16:05:11 +0000

    Corretti test

--- DBM2013/ext-matlab/svd_IR.m
+++ DBM2013/ext-matlab/svd_IR.m
@@ -4,6 +4,7 @@ functionpath = strcat('../data/',filename);
 X = csvread(functionpath);
 [U,S,V] = svd(X);
 %csvwrite(strcat('../data/U_',filename),U);
-csvwrite(strcat('../data/S_',filename),S(1:5,:));
-csvwrite(strcat('../data/V_',filename),V'(1:5,:));
+csvwrite(strcat('../data/S_',filename),S);
+V = V';
+csvwrite(strcat('../data/V_',filename),V(1:5,:));
 end
