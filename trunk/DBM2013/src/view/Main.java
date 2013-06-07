9f1e72ebb18793df0ec194c90a039a741f58e3c0
luca.violanti@gmail.com <luca.violanti@gmail.com@22b074c5-2d81-8dd0-1a27-45971b5c5bce>
2013-06-07 21:55:07 +0000

    Minor fix

--- trunk/DBM2013/src/view/Main.java
+++ trunk/DBM2013/src/view/Main.java
@@ -10,10 +10,7 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         Application.launch(Main.class, (java.lang.String[])null);
     }
 
