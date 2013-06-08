c29351c905738f2d8afc564c5e8b5c53b200d753
diego.rullo@gmail.com <diego.rullo@gmail.com@22b074c5-2d81-8dd0-1a27-45971b5c5bce>
2013-06-08 11:07:36 +0000

    prova inizializzazione interfaccia grafica (non funziona)

--- trunk/DBM2013/src/view/Main.java
+++ trunk/DBM2013/src/view/Main.java
@@ -1,10 +1,13 @@
 package view;
 
+import java.awt.TextField;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -22,9 +25,30 @@ public class Main extends Application {
             primaryStage.setScene(scene);
             primaryStage.setTitle("Progetto DBM 2013");
             primaryStage.show();
+            
+
         } catch (Exception ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    
+	@FXML
+	// fx:id="task1PaperIDTextField"
+	private TextField task1PaperIDTextField; // Valore iniettato attraverso FXMLLoader
+ 
+	@FXML
+	// fx:id="task1ModelloComboBox"
+	private ComboBox<String> task1ModelloComboBox; // Valore iniettato attraverso FXMLLoader
+ 
+	@FXML // Metodo chiamato attraverso FXMLLoader a inizializzazione completata
+    void initialize() {    
+        assert task1PaperIDTextField != null : "fx:id=\"task1PaperIDTextField\" non iniettato: controlla il file FXML 'dbm2013gui.fxml'.";
+        assert task1ModelloComboBox != null : "fx:id=\"task1ModelloComboBox\" non iniettato: controlla il file FXML 'dbm2013gui.fxml'.";
+
+        task1ModelloComboBox.getItems().clear();
+        task1ModelloComboBox.getItems().addAll("TF","TFIDF");
+        
+        task1PaperIDTextField.setText("<inserire paperid>");
+	}
 }
 
