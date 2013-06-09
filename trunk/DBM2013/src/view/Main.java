76de9283d1380787f210d19f226dc75315421cb3
luca.violanti@gmail.com <luca.violanti@gmail.com@22b074c5-2d81-8dd0-1a27-45971b5c5bce>
2013-06-09 09:15:40 +0000

    Rimossi warning

--- trunk/DBM2013/src/view/Main.java
+++ trunk/DBM2013/src/view/Main.java
@@ -3,13 +3,10 @@ package view;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -24,14 +21,11 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-        	FXMLLoader fxmlLoader = new FXMLLoader();
             AnchorPane page = (AnchorPane) FXMLLoader.load(Main.class.getResource("dbm2013gui.fxml"));
             Scene scene = new Scene(page);
             primaryStage.setScene(scene);
             primaryStage.setTitle("Progetto DBM 2013");
-            primaryStage.show();
-            
-        
+            primaryStage.show();        
 
         } catch (Exception ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
