c4682371cc08bdcad655455184ae8aa980180e2d
luca.violanti@gmail.com <luca.violanti@gmail.com@22b074c5-2d81-8dd0-1a27-45971b5c5bce>
2013-06-10 13:24:50 +0000

    Divisi file .m per SVD a seconda della matrice risultato di interesse (U o V'); Corretti metodi e test di conseguenza

---
+++ trunk/DBM2013/ext-matlab/svd_U.m
@@ -0,0 +1,19 @@
+function svd_IR(filename)
+	%pwd
+	functionpath = strcat('../data/',filename);
+	X = csvread(functionpath);
+	[U,S,V] = svd(X);
+	csvwrite(strcat('../data/U_',filename),U);
+	%csvwrite(strcat('../data/S_',filename),S);
+	%V = V';
+	%csvwrite(strcat('../data/V_',filename),V);
+	
+	% limitiamo la scrittura di U al massimo numero di righe presenti
+	% nel caso questo sia minore di 3.
+	[num_r, num_c] = size(U);
+	limit = 3;
+	if(num_r < 3)
+		limit = num_r;
+	end
+	csvwrite(strcat('../data/U_',filename),U(1:limit,:));
+end
