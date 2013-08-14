adb7aee16a719e82224a6f67afdd37a9c3693115
luca.violanti <luca.violanti@gmail.com>
2013-08-14 21:27:40 +0000

    Abbozzato il task 2 della fase 3: - rimane da calcolare il peso dell'arco; - il grafo risulta densissimo ed illeggibile (per questo non c'è molto da fare...)

--- DBM2013/test/utils/GraphEngineTest.java
+++ DBM2013/test/utils/GraphEngineTest.java
@@ -36,84 +36,169 @@ import exceptions.NoAuthorsWithSuchIDException;
 
 public class GraphEngineTest {
 
+	private final static boolean DEBUG = true;
+	
 	@SuppressWarnings("rawtypes") //FIXME: vedere come risolvere i warning
 	@Test
 	public void testGetCoAuthorsGraphBasedOnKeywordVectors() throws SQLException, MatlabConnectionException, MatlabInvocationException, IOException, NoAuthorsWithSuchIDException {
-		Factory factory = new Factory();
-        Corpus dblp = factory.getCorpus();
-        
-        Graph graph = dblp.getCoAuthorsGraphBasedOnKeywordVectors();
-        
-        //Count nodes and edges
-        System.out.println("Nodes: " + graph.getNodeCount() + " Edges: " + graph.getEdgeCount());
-        //Iterate over nodes
-        for(Node n : graph.getNodes()) {
-	        Node[] coauthors = graph.getNeighbors(n).toArray();
-	        System.out.println(n.getNodeData().getLabel() + " has " + coauthors.length + " coauthors");
-        }
-        
-//      //Find node by id
-//      Node node2 = directedGraph.getNode("n2");
-//      //Modify the graph while reading
-//      //Due to locking, you need to use toArray() on Iterable to be able to modify the graph in a read loop
-//      for(Node n : directedGraph.getNodes().toArray()) {
-//      	directedGraph.removeNode(n);
-//      }
-        
-        GraphEngine ge = GraphEngine.getGraphEngine();
-        GraphModel graphModel = ge.getGraphModel();
-        AttributeModel attributeModel = Lookup.getDefault().lookup(AttributeController.class).getModel();
-
-      RankingController rankingController = Lookup.getDefault().lookup(RankingController.class);
-      
-        //Rank color by Degree
-        Ranking degreeRanking = rankingController.getModel().getRanking(Ranking.NODE_ELEMENT, Ranking.DEGREE_RANKING);
-        AbstractColorTransformer colorTransformer = (AbstractColorTransformer) rankingController.getModel().getTransformer(Ranking.NODE_ELEMENT, Transformer.RENDERABLE_COLOR);
-        
-        colorTransformer.setColors(new Color[]{new Color(0x66CCCC), new Color(0x7EB6FF)});
-        rankingController.transform(degreeRanking, colorTransformer);
-
-        //Get Centrality
-        GraphDistance distance = new GraphDistance();
-        distance.setDirected(true);
-        distance.execute(graphModel, attributeModel);
-
-        //Rank size by centrality
-        AttributeColumn centralityColumn = attributeModel.getNodeTable().getColumn(GraphDistance.BETWEENNESS);
-        Ranking centralityRanking = rankingController.getModel().getRanking(Ranking.NODE_ELEMENT, centralityColumn.getId());
-        AbstractSizeTransformer sizeTransformer = (AbstractSizeTransformer) rankingController.getModel().getTransformer(Ranking.NODE_ELEMENT, Transformer.RENDERABLE_SIZE);
-        sizeTransformer.setMinSize(5);
-        sizeTransformer.setMaxSize(30);
-        rankingController.transform(centralityRanking, sizeTransformer);
-
-        //Rank label size - set a multiplier size
-        Ranking centralityRanking2 = rankingController.getModel().getRanking(Ranking.NODE_ELEMENT, centralityColumn.getId());
-        AbstractSizeTransformer labelSizeTransformer = (AbstractSizeTransformer) rankingController.getModel().getTransformer(Ranking.NODE_ELEMENT, Transformer.LABEL_SIZE);
-        labelSizeTransformer.setMinSize(1);
-        labelSizeTransformer.setMaxSize(3);
-        rankingController.transform(centralityRanking2,labelSizeTransformer);
-
-        //Set 'show labels' option in Preview - and disable node size influence on text size
-        PreviewModel previewModel = Lookup.getDefault().lookup(PreviewController.class).getModel();
-        previewModel.getProperties().putValue(PreviewProperty.EDGE_COLOR, new EdgeColor(new Color(0x7EB6FF)));
-        previewModel.getProperties().putValue(PreviewProperty.EDGE_LABEL_COLOR, new DependantOriginalColor(new Color(0x101010)));
-        
-        previewModel.getProperties().putValue(PreviewProperty.SHOW_NODE_LABELS, Boolean.TRUE);
-        previewModel.getProperties().putValue(PreviewProperty.SHOW_EDGE_LABELS, Boolean.TRUE);
-        previewModel.getProperties().putValue(PreviewProperty.NODE_LABEL_PROPORTIONAL_SIZE, Boolean.FALSE);
-
-        //Export
-        ExportController ec = Lookup.getDefault().lookup(ExportController.class);
-        try {
-        	File pdfFile = new File("coAuthorsGraphBasedOnKeywordVectors.pdf");
-        	File gexfFile = new File("coAuthorsGraphBasedOnKeywordVectors.gexf");
-        	ec.exportFile(pdfFile); //ec.exportFile(new File("coAuthorsGraphBasedOnKeywordVectors.pdf"));
-        	ec.exportFile(gexfFile);
-            Desktop.getDesktop().open(pdfFile);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            return;
-        }
-        
+		if(!DEBUG) {
+			Factory factory = new Factory();
+	        Corpus dblp = factory.getCorpus();
+	        
+	        Graph graph = dblp.getCoAuthorsGraphBasedOnKeywordVectors();
+	        
+	        //Count nodes and edges
+	        System.out.println("Nodes: " + graph.getNodeCount() + " Edges: " + graph.getEdgeCount());
+	        //Iterate over nodes
+	        for(Node n : graph.getNodes()) {
+		        Node[] coauthors = graph.getNeighbors(n).toArray();
+		        System.out.println(n.getNodeData().getLabel() + " has " + coauthors.length + " coauthors");
+	        }
+	        
+	//      //Find node by id
+	//      Node node2 = directedGraph.getNode("n2");
+	//      //Modify the graph while reading
+	//      //Due to locking, you need to use toArray() on Iterable to be able to modify the graph in a read loop
+	//      for(Node n : directedGraph.getNodes().toArray()) {
+	//      	directedGraph.removeNode(n);
+	//      }
+	        
+	        GraphEngine ge = GraphEngine.getGraphEngine();
+	        GraphModel graphModel = ge.getGraphModel();
+	        AttributeModel attributeModel = Lookup.getDefault().lookup(AttributeController.class).getModel();
+	
+	      RankingController rankingController = Lookup.getDefault().lookup(RankingController.class);
+	      
+	        //Rank color by Degree
+	        Ranking degreeRanking = rankingController.getModel().getRanking(Ranking.NODE_ELEMENT, Ranking.DEGREE_RANKING);
+	        AbstractColorTransformer colorTransformer = (AbstractColorTransformer) rankingController.getModel().getTransformer(Ranking.NODE_ELEMENT, Transformer.RENDERABLE_COLOR);
+	        
+	        colorTransformer.setColors(new Color[]{new Color(0x66CCCC), new Color(0x7EB6FF)});
+	        rankingController.transform(degreeRanking, colorTransformer);
+	
+	        //Get Centrality
+	        GraphDistance distance = new GraphDistance();
+	        distance.setDirected(true);
+	        distance.execute(graphModel, attributeModel);
+	
+	        //Rank size by centrality
+	        AttributeColumn centralityColumn = attributeModel.getNodeTable().getColumn(GraphDistance.BETWEENNESS);
+	        Ranking centralityRanking = rankingController.getModel().getRanking(Ranking.NODE_ELEMENT, centralityColumn.getId());
+	        AbstractSizeTransformer sizeTransformer = (AbstractSizeTransformer) rankingController.getModel().getTransformer(Ranking.NODE_ELEMENT, Transformer.RENDERABLE_SIZE);
+	        sizeTransformer.setMinSize(5);
+	        sizeTransformer.setMaxSize(30);
+	        rankingController.transform(centralityRanking, sizeTransformer);
+	
+	        //Rank label size - set a multiplier size
+	        Ranking centralityRanking2 = rankingController.getModel().getRanking(Ranking.NODE_ELEMENT, centralityColumn.getId());
+	        AbstractSizeTransformer labelSizeTransformer = (AbstractSizeTransformer) rankingController.getModel().getTransformer(Ranking.NODE_ELEMENT, Transformer.LABEL_SIZE);
+	        labelSizeTransformer.setMinSize(1);
+	        labelSizeTransformer.setMaxSize(3);
+	        rankingController.transform(centralityRanking2,labelSizeTransformer);
+	
+	        //Set 'show labels' option in Preview - and disable node size influence on text size
+	        PreviewModel previewModel = Lookup.getDefault().lookup(PreviewController.class).getModel();
+	        previewModel.getProperties().putValue(PreviewProperty.EDGE_COLOR, new EdgeColor(new Color(0x7EB6FF)));
+	        previewModel.getProperties().putValue(PreviewProperty.EDGE_LABEL_COLOR, new DependantOriginalColor(new Color(0x101010)));
+	        
+	        previewModel.getProperties().putValue(PreviewProperty.SHOW_NODE_LABELS, Boolean.TRUE);
+	        previewModel.getProperties().putValue(PreviewProperty.SHOW_EDGE_LABELS, Boolean.TRUE);
+	        previewModel.getProperties().putValue(PreviewProperty.NODE_LABEL_PROPORTIONAL_SIZE, Boolean.FALSE);
+	
+	        //Export
+	        ExportController ec = Lookup.getDefault().lookup(ExportController.class);
+	        try {
+	        	File pdfFile = new File("[Phase 3 - Task 1] coAuthorsGraphBasedOnKeywordVectors.pdf");
+	        	File gexfFile = new File("[Phase 3 - Task 1] coAuthorsGraphBasedOnKeywordVectors.gexf");
+	        	ec.exportFile(pdfFile); //ec.exportFile(new File("[Phase 3 - Task 1] coAuthorsGraphBasedOnKeywordVectors.pdf"));
+	        	ec.exportFile(gexfFile);
+	            Desktop.getDesktop().open(pdfFile);
+	        } catch (IOException ex) {
+	            ex.printStackTrace();
+	            return;
+	        }
+		}        
+	}
+	
+	@SuppressWarnings("rawtypes") //FIXME: vedere come risolvere i warning
+	@Test
+	public void testGetCoAuthoredPapersGraphBasedOnKeywordVectors() throws SQLException, MatlabConnectionException, MatlabInvocationException, IOException, NoAuthorsWithSuchIDException {
+		if(DEBUG) {
+			Factory factory = new Factory();
+	        Corpus dblp = factory.getCorpus();
+	        
+	        Graph graph = dblp.getCoAuthoredPapersGraphBasedOnKeywordVectors();
+	        System.out.println("ciao");
+	        //Count nodes and edges
+	        System.out.println("Nodes: " + graph.getNodeCount() + " Edges: " + graph.getEdgeCount());
+	        //Iterate over nodes
+	        for(Node n : graph.getNodes()) {
+		        Node[] coauthors = graph.getNeighbors(n).toArray();
+		        System.out.println(n.getNodeData().getLabel() + " has " + coauthors.length + " coauthors");
+	        }
+	        
+	//      //Find node by id
+	//      Node node2 = directedGraph.getNode("n2");
+	//      //Modify the graph while reading
+	//      //Due to locking, you need to use toArray() on Iterable to be able to modify the graph in a read loop
+	//      for(Node n : directedGraph.getNodes().toArray()) {
+	//      	directedGraph.removeNode(n);
+	//      }
+	        
+	        GraphEngine ge = GraphEngine.getGraphEngine();
+	        GraphModel graphModel = ge.getGraphModel();
+	        AttributeModel attributeModel = Lookup.getDefault().lookup(AttributeController.class).getModel();
+	
+	      RankingController rankingController = Lookup.getDefault().lookup(RankingController.class);
+	      
+	        //Rank color by Degree
+	        Ranking degreeRanking = rankingController.getModel().getRanking(Ranking.NODE_ELEMENT, Ranking.DEGREE_RANKING);
+	        AbstractColorTransformer colorTransformer = (AbstractColorTransformer) rankingController.getModel().getTransformer(Ranking.NODE_ELEMENT, Transformer.RENDERABLE_COLOR);
+	        
+	        colorTransformer.setColors(new Color[]{new Color(0x66CCCC), new Color(0x7EB6FF)});
+	        rankingController.transform(degreeRanking, colorTransformer);
+	
+	        //Get Centrality
+	        GraphDistance distance = new GraphDistance();
+	        distance.setDirected(true);
+	        distance.execute(graphModel, attributeModel);
+	
+	        //Rank size by centrality
+	        AttributeColumn centralityColumn = attributeModel.getNodeTable().getColumn(GraphDistance.BETWEENNESS);
+	        Ranking centralityRanking = rankingController.getModel().getRanking(Ranking.NODE_ELEMENT, centralityColumn.getId());
+	        AbstractSizeTransformer sizeTransformer = (AbstractSizeTransformer) rankingController.getModel().getTransformer(Ranking.NODE_ELEMENT, Transformer.RENDERABLE_SIZE);
+	        sizeTransformer.setMinSize(5);
+	        sizeTransformer.setMaxSize(30);
+	        rankingController.transform(centralityRanking, sizeTransformer);
+	
+	        //Rank label size - set a multiplier size
+	        Ranking centralityRanking2 = rankingController.getModel().getRanking(Ranking.NODE_ELEMENT, centralityColumn.getId());
+	        AbstractSizeTransformer labelSizeTransformer = (AbstractSizeTransformer) rankingController.getModel().getTransformer(Ranking.NODE_ELEMENT, Transformer.LABEL_SIZE);
+	        labelSizeTransformer.setMinSize(1);
+	        labelSizeTransformer.setMaxSize(3);
+	        rankingController.transform(centralityRanking2,labelSizeTransformer);
+	
+	        //Set 'show labels' option in Preview - and disable node size influence on text size
+	        PreviewModel previewModel = Lookup.getDefault().lookup(PreviewController.class).getModel();
+	        previewModel.getProperties().putValue(PreviewProperty.EDGE_COLOR, new EdgeColor(new Color(0x7EB6FF)));
+	        previewModel.getProperties().putValue(PreviewProperty.EDGE_LABEL_COLOR, new DependantOriginalColor(new Color(0x101010)));
+	        
+	        previewModel.getProperties().putValue(PreviewProperty.SHOW_NODE_LABELS, Boolean.TRUE);
+	        previewModel.getProperties().putValue(PreviewProperty.SHOW_EDGE_LABELS, Boolean.TRUE);
+	        previewModel.getProperties().putValue(PreviewProperty.NODE_LABEL_PROPORTIONAL_SIZE, Boolean.FALSE);
+	
+	        //Export
+	        ExportController ec = Lookup.getDefault().lookup(ExportController.class);
+	        try {
+	        	File pdfFile = new File("[Phase 3 - Task 2] coAuthoredPapersGraphBasedOnKeywordVectors.pdf");
+	        	File gexfFile = new File("[Phase 3 - Task 2] coAuthoredPapersGraphBasedOnKeywordVectors.gexf");
+	        	ec.exportFile(pdfFile); //ec.exportFile(new File("[Phase 3 - Task 2] coAuthoredPapersGraphBasedOnKeywordVectors.pdf"));
+	        	ec.exportFile(gexfFile);
+	            Desktop.getDesktop().open(pdfFile);
+	        } catch (IOException ex) {
+	            ex.printStackTrace();
+	            return;
+	        }
+		}
 	}
 }
